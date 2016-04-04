a30e156c0623505d8061c5d433d60e42f04c18b8
Jeremy Chui <jeremy@jchui.me>
2016-04-04 11:32:17 +0100

    Added options on settings page
    Only stores settings in session, not locally at the moment.

--- js/app.js
+++ js/app.js
@@ -17,10 +17,36 @@ angular.module('myApp', ['ui.router'])
     });
 })
 
-.controller("HomeCtrl", function($scope, $http, $timeout) {
+.factory('UserService', function() {
+  var defaults = {
+  	apikey: 'API_KEY_HERE',
+    phase: '3b'
+  };
+ 
+  var service = {
+    user: {},
+    save: function() {
+      sessionStorage.newsfeed =
+        angular.toJson(service.user);
+    },
+    restore: function() {
+      // Pull from sessionStorage
+      service.user = 
+        angular.fromJson(sessionStorage.newsfeed) || defaults
+ 
+      return service.user;
+    }
+  };
+  // Immediately call restore from the session storage
+  // so we have our user data available immediately
+  service.restore();
+  return service;
+})
+
+.controller("HomeCtrl", function($scope, $http, $timeout, UserService) {
 	
 	$scope.loading = true;
-	
+	$scope.user = UserService.user;
 	$scope.date = {};
  
 	var updateTime = function() {
@@ -30,8 +56,8 @@ angular.module('myApp', ['ui.router'])
 	
 	updateTime();
     
-    var apikey = 'API_KEY_HERE';
-    var phase = '3b';
+    var apikey = $scope.user.apikey;
+    var phase = $scope.user.phase;
     
     $http.get('http://api.jchui.me/minerva/?apikey=' + apikey + '&phase=' + phase).
 		success(function(data, status, headers, config) {
@@ -47,6 +73,12 @@ angular.module('myApp', ['ui.router'])
 })
 
 .controller('SettingsCtrl',
-  function($scope) {
-    // Settings stuff
+  function($scope, UserService) {
+   
+	$scope.user = UserService.user;
+	
+	$scope.save = function() {
+      UserService.save();
+    }   
+   
 });
